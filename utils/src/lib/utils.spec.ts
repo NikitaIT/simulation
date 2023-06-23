6e558ba256cf2e150b8c190bf04a068561612bbc
NikitaIT <nikita2008101@gmail.com>
2023-06-23 12:48:04 +0200

    feat(utils): add timeout and delay

--- utils/src/lib/utils.spec.ts
+++ utils/src/lib/utils.spec.ts
@@ -1,7 +1,26 @@
-import { utils } from './utils';
-
-describe('utils', () => {
-  it('should work', () => {
-    expect(utils()).toEqual('utils');
-  });
+import { timeout, delay } from './utils';
+describe('Given utils.timeout and utils.delay race', () => {
+  it.each([
+    {
+      timeout: 10,
+      delay: 5,
+      expected: 'delay',
+    },
+    {
+      timeout: 5,
+      delay: 10,
+      expected: 'timeout',
+    },
+  ])(
+    'When timeout=$timeout delay=$delay Then result should be $expected',
+    async (config) => {
+      const ac = new AbortController();
+      const result = await Promise.race([
+        timeout(config.timeout, ac.signal).catch(() => 'timeout'),
+        delay(config.delay).then(() => 'delay'),
+      ]);
+      ac.abort(); // stop one of steel not resolved/rejected promises
+      expect(result).toEqual(config.expected);
+    }
+  );
 });
--- utils/src/lib/utils.ts
+++ utils/src/lib/utils.ts
@@ -1,3 +1,44 @@
-export function utils(): string {
-  return 'utils';
-}
+/**
+ * Reject if X msec expired or signal aborted
+ * @example
+ * 
+      const ac = new AbortController();
+      result = await Promise.race([
+        timeout(timeoutMsec, ac.signal),
+        fetch(args, { signal: ac.signal }),
+      ]);
+      ac.abort();
+ * @param msec msec
+ * @param signal AbortSignal
+ * @returns Promise rejected after X msec or after abort signal
+ */
+export const timeout = (msec: number, signal?: AbortSignal) =>
+  new Promise((resolve, reject) => {
+    const timer = setTimeout(() => {
+      reject(new Error(`Timeout of ${msec}ms reached`));
+    }, msec);
+    if (!signal) return;
+    signal.addEventListener('abort', () => {
+      clearTimeout(timer);
+      reject(new Error('Timeout aborted'));
+    });
+  });
+
+/**
+ * Wait for X msec if not aborted
+ * @example
+ * 
+      await delay(delayMsec);
+ * @param msec msec
+ * @param signal AbortSignal
+ * @returns Promise resolved after X msec or reject after abort signal
+ */
+export const delay = (msec: number, signal?: AbortSignal) =>
+  new Promise((resolve, reject) => {
+    const timer = setTimeout(resolve, msec);
+    if (!signal) return;
+    signal.addEventListener('abort', () => {
+      clearTimeout(timer);
+      reject(new Error('Delay aborted'));
+    });
+  });
