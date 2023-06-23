86767af81c78d10f69cef5332f08a0d9a3998a8f
NikitaIT <nikita2008101@gmail.com>
2023-06-23 19:21:43 +0200

    test(nba-node): add 5 clients test

--- src/app/routes/stream/route.e2e.spec.ts
+++ src/app/routes/stream/route.e2e.spec.ts
@@ -16,9 +16,33 @@ describe('Route E2E', () => {
     con?.destroy();
     client?.destroy();
   });
-  it('Should expose a websocket', async () => {
+  it('Should works as expected with single client', async () => {
     const isLogged = false;
 
+    fastify = Fastify({
+      // logger: {
+      //   transport: {
+      //     target: 'pino-pretty',
+      //     options: {
+      //       translateTime: 'HH:MM:ss Z',
+      //       ignore: 'pid,hostname',
+      //     },
+      //   },
+      // },
+    });
+
+    await fastify.register(websocketPlugin);
+    await route(fastify, {
+      intervalSec: 0.1,
+      timeoutSec: 0.9,
+    });
+
+    await fastify.listen({ port: 0 /* 0 means any port*/ });
+
+    await spawnClient(fastify, isLogged);
+  }, 6000);
+
+  it('5 nonclustered clients in same context', async () => {
     fastify = Fastify({
       // logger: {
       //   transport: {
@@ -39,50 +63,12 @@ describe('Route E2E', () => {
     });
 
     await fastify.listen({ port: 0 /* 0 means any port*/ });
-    const ws = new WebSocket(
-      'ws://localhost:' + (fastify.server.address() as any).port + '/'
+
+    await Promise.all(
+      Array.from(new Array(5), (x, i) => i).map((x) =>
+        spawnClient(fastify, false)
+      )
     );
-
-    let messages: string[] = [];
-    ws.on('open', async () => {
-      console.log('open');
-      const sender = new Sender(ws);
-      sender.send(new Start({ matchIds: ['1', '2', '3'] }));
-      await delay(250).then(() => {
-        isLogged && console.log('Start 250, expect 6', messages);
-        expect(messages.length).toEqual(6);
-        messages = [];
-      });
-      sender.send(new Stop({ matchIds: ['1', '2', '3'] }));
-      await delay(200).then(() => {
-        isLogged && console.log('Stop 200, expect 0', messages);
-        expect(messages.length).toEqual(0);
-        messages = [];
-      });
-      sender.send(new Start({ matchIds: ['1', '2', '3'] }));
-      await delay(110).then(() => {
-        isLogged && console.log('Start 100, expect 3', messages);
-        expect(messages.length).toEqual(3);
-        messages = [];
-      });
-      sender.send(new Stop({ matchIds: ['1', '2', '3'] }));
-      await delay(200).then(() => {
-        isLogged && console.log('Stop 200, expect 0', messages);
-        expect(messages.length).toEqual(0);
-        messages = [];
-      });
-      sender.send(new Restart({ matchIds: ['1', '2', '3'] }));
-      await delay(900);
-    });
-    ws.on('message', (x) => messages.push(x.toString()));
-    ws.on('close', () => {
-      // console.log('close after Restart', messages);
-      expect(messages.length).toEqual(27);
-    });
-    await delay(3000);
-
-    ws.close();
-    await delay(1000);
   }, 6000);
   it('The API server should expose a stream endpoint', () => {
     // ok
@@ -100,4 +86,53 @@ class Sender {
   send(type: unknown) {
     this.ws.send(JSON.stringify(type));
   }
+}
+
+async function spawnClient(fastify: FastifyInstance, isLogged: boolean) {
+  const ws = new WebSocket(
+    'ws://localhost:' + (fastify.server.address() as any).port + '/'
+  );
+
+  let messages: string[] = [];
+  ws.on('open', async () => {
+    console.log('open');
+    const sender = new Sender(ws);
+    sender.send(new Start({ matchIds: ['1', '2', '3'] }));
+    await delay(250).then(() => {
+      isLogged && console.log('Start 250, expect 6', messages);
+      expect(messages.length).toEqual(6);
+      messages = [];
+    });
+    sender.send(new Stop({ matchIds: ['1', '2', '3'] }));
+    await delay(200).then(() => {
+      isLogged && console.log('Stop 200, expect 0', messages);
+      expect(messages.length).toEqual(0);
+      messages = [];
+    });
+    sender.send(new Start({ matchIds: ['1', '2', '3'] }));
+    await delay(110).then(() => {
+      isLogged && console.log('Start 100, expect 3', messages);
+      expect(messages.length).toEqual(3);
+      messages = [];
+    });
+    sender.send(new Stop({ matchIds: ['1', '2', '3'] }));
+    await delay(200).then(() => {
+      isLogged && console.log('Stop 200, expect 0', messages);
+      expect(messages.length).toEqual(0);
+      messages = [];
+    });
+    sender.send(new Restart({ matchIds: ['1', '2', '3'] }));
+    await delay(900);
+  });
+  ws.on('message', (x) => messages.push(x.toString()));
+  ws.on('close', () => {
+    // console.log('close after Restart', messages);
+    expect(messages.length).toEqual(27);
+  });
+  ws.on('error', () => console.log('errored'));
+
+  await delay(3000);
+
+  ws.close();
+  await delay(1000);
 }
